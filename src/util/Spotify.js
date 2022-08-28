83d8812f9b4c6542da8aa979910386ae8c1cd41d
r.bonk907@gmail.com <r.bonk907@gmail.com>
2022-08-28 10:39:03 -0800

    refactor access token get method

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -2,21 +2,26 @@ let userAccessToken = '';
 const clientID = '294959fa3089485a9779510d956b0a88';
 const redirectURI = 'http://localhost:3000/';
 
+function setTokenExpiration(token, expiration) {
+    userAccessToken = token[1];
+    const expirationTime = expiration[1];
+    window.setTimeout(() => userAccessToken = '', expirationTime * 1000);
+    window.history.pushState('Access Token', null, '/');   
+}
 
 const Spotify = {
     getAccessToken() {
         if(userAccessToken) {
+            console.log('User access token already acquired, returning...');
             return userAccessToken;
         }
         
+        console.log('User access token not acquired yet. Parsing... ');
         let accessToken = window.location.href.match(/access_token=([^&]*)/);
         let expiresIn = window.location.href.match(/expires_in=([^&]*)/);
 
         if (accessToken && expiresIn) {
-            userAccessToken = accessToken[1];
-            const expirationTime = expiresIn[1];
-            window.setTimeout(() => userAccessToken = '', expirationTime * 1000);
-            window.history.pushState('Access Token', null, '/');
+            setTokenExpiration(accessToken, expiresIn);
 
             return userAccessToken;
         }
