fb3b7558b3ed0f24a01485f29c45cd5ed8b574c3
rbonk907 <40987927+rbonk907@users.noreply.github.com>
2022-09-03 13:09:35 -0800

    store current search term on user redirect

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -19,11 +19,33 @@ class SearchBar extends React.Component {
     handleTermChange(event) {
         this.setState({ searchTerm: event.target.value })
     }
+
+    componentDidMount() {
+        /*
+        /  When the SearchBar component mounts, check to see if there is a search term
+        /  saved in the session storage. If so, update the state of the search bar to
+        /  reflect that. 
+        /  
+        /  Due to this component mounting twice, we could either remove
+        /  strict mode, which prevents mounting twice or we could perform
+        /  a bit of latching so the state.searchTerm is only updated if it
+        /  was previoulsy empty 
+        */
+        if(sessionStorage.getItem('savedSearchTerm')) {
+            const savedSearch = sessionStorage.getItem('savedSearchTerm');
+            this.setState({ searchTerm: savedSearch });
+        }
+    }
     
     render() {
+        if(this.state.searchTerm === sessionStorage.getItem('savedSearchTerm')) {
+            this.search();
+        }
+
+
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} value={this.state.searchTerm}/>
                 <button className="SearchButton" onClick={this.search}>SEARCH</button>
             </div>
         );
